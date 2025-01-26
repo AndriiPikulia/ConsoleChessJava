fccfe1d897dcec8723b8eee9856adb90681b8603
Andrii Pikulia <andrewpikulia@gmail.com>
2025-01-26 20:50:56 +0200

    [board] added better visuals for board

--- ChessGame/src/ChessView.java
+++ ChessGame/src/ChessView.java
@@ -1,11 +1,27 @@
 public class ChessView {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
     public void printBoard(char[][] board) {
-        for (int i = 0; i <8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.print("\n");
+        System.out.print("  + ");
+        for(int j = 0; j < 8; j++) {
+            System.out.print("- ");
         }
+        System.out.println("+");
+        for (int i = 0; i <8; i++) {
+            System.out.print(ANSI_BLUE + (i+1) + ANSI_RESET + " | ");
+            for (int j = 0; j < 8; j++) {
+                System.out.printf(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.print("  + ");
+        for(int j = 0; j < 8; j++) {
+            System.out.print("- ");
+        }
+        System.out.println("+");
+        System.out.println(ANSI_BLUE + "    a b c d e f g h " + ANSI_RESET);
     }
+
 }
 
