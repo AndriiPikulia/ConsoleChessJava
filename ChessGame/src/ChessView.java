dcc8c8b550a2de88cfb81ea94d22d22c103c7ab1
AndriiPikulia <145601915+AndriiPikulia@users.noreply.github.com>
2025-01-27 00:14:24 +0200

    added better visuals for board
    * [board] added better visuals for board
    
    * [board] added more colors, painted black figures in blue color
    
    ---------
    
    Co-authored-by: Andrii Pikulia <andrewpikulia@gmail.com>

--- ChessGame/src/ChessView.java
+++ ChessGame/src/ChessView.java
@@ -1,11 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChessView {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
     public void printBoard(char[][] board) {
-        for (int i = 0; i <8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.print("\n");
+        System.out.print("  + ");
+        for(int j = 0; j < 8; j++) {
+            System.out.print("- ");
         }
+        System.out.println("+");
+        for (int i = 0; i <8; i++) {
+            System.out.print(ANSI_YELLOW + (i+1) + ANSI_RESET + " | ");
+            for (int j = 0; j < 8; j++) {
+                if(!Character.isUpperCase(board[i][j]) && board[i][j] != '.') {
+                    System.out.printf(ANSI_BLUE + board[i][j] + ANSI_RESET + " ");
+                }
+                else {
+                    System.out.print(board[i][j]+ " ");
+                }
+            }
+            System.out.println("|");
+        }
+        System.out.print("  + ");
+        for(int j = 0; j < 8; j++) {
+            System.out.print("- ");
+        }
+        System.out.println("+");
+        System.out.println(ANSI_YELLOW + "    a b c d e f g h " + ANSI_RESET);
     }
+
 }
 
